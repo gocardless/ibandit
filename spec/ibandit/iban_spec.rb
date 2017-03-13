4487278dac16b1f4587025909e5eed23c207b0f7
Thomas Whaples <twhaples@gocardless.com>
2017-03-13 14:31:00 +0000

    Slovenia uses 5-digit national IDs
    (I'm not sure why the input file says otherwise.)

--- spec/ibandit/iban_spec.rb
+++ spec/ibandit/iban_spec.rb
@@ -88,6 +88,19 @@ describe Ibandit::IBAN do
       its(:local_check_digits) { is_expected.to be_nil }
     end
 
+    context 'when the IBAN was created from a Slovenian IBAN' do
+      let(:iban_code) { 'SI56 1910 0000 0123 438' }
+      its(:country_code) { is_expected.to eq('SI') }
+      its(:bank_code) { is_expected.to eq('19100') }
+      its(:branch_code) { is_expected.to be_nil }
+      its(:account_number) { is_expected.to eq('0000123438') }
+      its(:swift_bank_code) { is_expected.to eq('19100') }
+      its(:swift_branch_code) { is_expected.to be_nil }
+      its(:swift_account_number) { is_expected.to eq('0000123438') }
+      its(:swift_national_id) { is_expected.to eq('19100') }
+      its(:local_check_digits) { is_expected.to be_nil }
+    end
+
     context 'when the IBAN was created with local details' do
       let(:arg) do
         {
