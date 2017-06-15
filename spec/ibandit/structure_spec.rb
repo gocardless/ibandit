1bd1cacfd86ca7097a54bce4b071cb0bb849357d
Ernesto Jiménez <ernesto@gocardless.com>
2017-06-16 00:23:03 +0100

    Add spec to check structure.yml regexps build

---
+++ spec/ibandit/structure_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'structures.yml' do
+  FILE = File.expand_path('../../../data/structures.yml', __FILE__)
+  STRUCTURES = YAML.load_file(FILE)
+
+  STRUCTURES.each do |country, rules|
+    context country do
+      rules.each do |rule, value|
+        next unless rule =~ /_format$/
+
+        it "builds #{rule} rule" do
+          expect { Regexp.new(value) }.to_not raise_exception
+        end
+      end
+    end
+  end
+end
