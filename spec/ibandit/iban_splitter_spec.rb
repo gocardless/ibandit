ba5cc3c0c266cd35ed6d618892b8f3af6e0ea6be
Nick Campbell <nickcampbell@gocardless.com>
2018-10-18 09:07:25 +0100

    Autocorrect Rubocop failures ($ rubocop -a)

--- spec/ibandit/iban_splitter_spec.rb
+++ spec/ibandit/iban_splitter_spec.rb
@@ -5,6 +5,7 @@ describe Ibandit::IBANSplitter do
 
   context "with a valid IBAN" do
     let(:iban_code) { "GB82WEST12345698765432" }
+
     its([:country_code]) { is_expected.to eq("GB") }
     its([:check_digits]) { is_expected.to eq("82") }
     its([:bank_code]) { is_expected.to eq("WEST") }
@@ -14,6 +15,7 @@ describe Ibandit::IBANSplitter do
 
   context "with nil" do
     let(:iban_code) { nil }
+
     its([:country_code]) { is_expected.to eq(nil) }
     its([:check_digits]) { is_expected.to eq(nil) }
     its([:bank_code]) { is_expected.to eq(nil) }
@@ -23,6 +25,7 @@ describe Ibandit::IBANSplitter do
 
   context "with an empty string" do
     let(:iban_code) { "" }
+
     its([:country_code]) { is_expected.to eq(nil) }
     its([:check_digits]) { is_expected.to eq(nil) }
     its([:bank_code]) { is_expected.to eq(nil) }
@@ -32,6 +35,7 @@ describe Ibandit::IBANSplitter do
 
   context "with an invalid length IBAN" do
     let(:iban_code) { "MC9320052222100112233M445" }
+
     its([:country_code]) { is_expected.to eq("MC") }
     its([:check_digits]) { is_expected.to eq(nil) }
     its([:bank_code]) { is_expected.to eq(nil) }
